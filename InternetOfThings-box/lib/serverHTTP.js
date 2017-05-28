1cd2ee032015054beb37aaa551c36db721d8152b
Nelson Gomes <nelsonmpg@gmail.com>
2017-05-28 23:22:50 +0100

    no message

--- InternetOfThings-box/lib/serverHTTP.js
+++ InternetOfThings-box/lib/serverHTTP.js
@@ -117,7 +117,7 @@ console.log('Server listening Tunnel SSH on local %s:%s and remote %s:%s'.blue.b
 ServerHTTP.prototype.createReverseTunnel = function(){  
   var self = this;
   // inicia o tunel ssh com a cloud
-  cp.exec("nohup sh ./runTunneling.sh " + self.tunnelssh.remoteport + " " +  self.tunnelssh.localip + " " + self.tunnelssh.localport + " " + self.tunnelssh.remoteuser + " '" + self.tunnelssh.remoteip + "'" + " &", function (error, stdout, stderr) {
+  cp.execSync("nohup sh ./runTunneling.sh " + self.tunnelssh.remoteport + " " +  self.tunnelssh.localip + " " + self.tunnelssh.localport + " " + self.tunnelssh.remoteuser + " '" + self.tunnelssh.remoteip + "'" + " &", function (error, stdout, stderr) {
     if (error instanceof Error) {
       console.log('exec error: ' + error);
       console.log("Erro na criação do tunel SHH port : %s:%s".red.bold, self.tunnelssh.remoteip, self.tunnelssh.remoteport);
