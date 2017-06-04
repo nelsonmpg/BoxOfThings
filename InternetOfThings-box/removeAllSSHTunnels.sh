e46d520d2269b17fa395a7066bc378b9cdcd8f1a
Nelson Gomes <nelsonmpg@gmail.com>
2017-06-04 15:20:27 +0100

    no message

---
+++ InternetOfThings-box/removeAllSSHTunnels.sh
@@ -0,0 +1,4 @@
+# verifica se  o tunnel existe se não executa o script para o criar
+for x in "$(ps aux | grep  "ssh -fN -R" | grep -v pts/0 |  tr -s " " " " | cut -d' ' -f2)"; do
+	sudo kill -9 $x
+done
