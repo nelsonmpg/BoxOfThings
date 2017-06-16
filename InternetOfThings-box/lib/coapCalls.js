3aa75f7dc3c2d0a1a37c0404fa07132c20e01f59
Nelson Gomes <nelsonmpg@gmail.com>
2017-06-16 21:23:05 +0100

    no message

--- InternetOfThings-box/lib/coapCalls.js
+++ InternetOfThings-box/lib/coapCalls.js
@@ -16,7 +16,7 @@ module.exports = {
         log.appendToLog('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
 
         sock.on('data', function(data) {
-            // console.log('DATA ' + sock.remoteAddress + ': ' + data);
+            console.log('DATA ' + sock.remoteAddress + ': ' + data);
             log.appendToLog('DATA ' + sock.remoteAddress + ': ' + data);
             try {
                 var req = JSON.parse(data);
