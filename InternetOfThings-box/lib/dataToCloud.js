6d296bf294fc75ea5f423a94c45e71da88ac4362
Nelson Gomes <nelsonmpg@gmail.com>
2017-07-13 09:51:54 +0100

    no message

--- InternetOfThings-box/lib/dataToCloud.js
+++ InternetOfThings-box/lib/dataToCloud.js
@@ -11,10 +11,11 @@ module.exports = {
          method: 'POST',
          form: dataFusionObj
      }
+     console.log(options);
 
         // Start the request
         request(options, function (error, response, body) {
-         if (!error && response.statusCode == 200) {
+           if (!error && response.statusCode == 200) {
                 // Print out the response body
                 console.log(body)
             }
