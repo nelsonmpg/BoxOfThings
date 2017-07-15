909422947165c794bac0e3b009caf427ed29e345
Nelson Gomes <nelsonmpg@gmail.com>
2017-07-15 05:54:05 +0100

    no message

--- exemplo-server-cloud.js
+++ exemplo-server-cloud.js
@@ -10,30 +10,32 @@ app.use(express.static('./public'));
 app.use(bodyParser.json());
 
 app.post("/sensors", function(req, res) {
-    console.log("Receive", req.body);
-    res.send({'status': 'sensors ok', 'stdout': new Date(), 'stderr': new Date() });
+    console.log("Receive '/sensors'", req.body);
+    res.send({'status': 'sensors ok', 'stdout': new Date()});
 });
 
 app.post("/sensorscheck", function(req, res) {
-    console.log("Receive", req.body);
-    res.send({ 'status': 'sensorscheck ok', 'stdout': new Date(), 'stderr': new Date() });
+    console.log("Receive '/sensorscheck'", req.body);
+    res.send({ 'status': 'sensorscheck ok', 'stdout': new Date()});
 });
+
 
 app.post("/boxes", function(req, res) {
     console.log("Receive", req.body);
     res.send({ 'status': 'boxes ok', 'stdout': new Date(), 'stderr': new Date()});
 });
 
-app.get('/boxes', function(req, res) {
-    res.send('Hello World! ' + new Date().toString() + ' - ' + process.env.PORT);
+app.post("/boxes", function(req, res) {
+    console.log("Receive '/boxes'", req.body);
+    res.send({ 'status': 'boxes ok', 'stdout': new Date()});
 });
 
 app.get('/sensors', function(req, res) {
-    res.send('Hello World! ' + new Date().toString() + ' - ' + process.env.PORT);
+    res.send("Hello World! '/sensors'" + new Date().toString() + ' - ' + process.env.PORT);
 });
 
 app.get('/sensorscheck', function(req, res) {
-    res.send('Hello World! ' + new Date().toString() + ' - ' + process.env.PORT);
+    res.send("Hello World! '/sensorscheck'" + new Date().toString() + ' - ' + process.env.PORT);
 });
 
 var port = process.env.PORT || 3000;
